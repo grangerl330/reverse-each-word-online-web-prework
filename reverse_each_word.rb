f292855bb7de592bf7bedc649d6028182023191f
Luke Granger <grangerl330@gmail.com>
2019-01-04 04:18:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  split_sentence = sentence.split("")
+  split_sentence = sentence.split(",")
   split_sentence.each do |word|
-    word.reverse
+    word.reverse!
   end
 end
